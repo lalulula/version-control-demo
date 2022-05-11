880c782f34feddf483dfd0a7d98b021c9126b962
sjchae <sjchae416@gmail.com>
2022-05-11 16:08:38 +0900

    git acticity - comment added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import './App.css';
-import {Route, Redirect, Switch} from 'react-router-dom';
-import HelloMessage from "./components/HelloMessage";
-import FunctionPage from "./components/FunctionPage";
-import ClassPage from "./components/ClassPage";
-import FormExample from "./components/FormExample";
-import ShowList from "./components/ShowList";
+import { Route, Redirect, Switch } from 'react-router-dom';
+import HelloMessage from './components/HelloMessage';
+import FunctionPage from './components/FunctionPage';
+import ClassPage from './components/ClassPage';
+import FormExample from './components/FormExample';
+import ShowList from './components/ShowList';
 
-const NoMatch = ({location}) => (
-    <div>
-        <strong>Error!</strong> No route found matching:
-        <div>
-            <code>{location.pathname}</code>
-        </div>
-    </div>
+const NoMatch = ({ location }) => (
+	<div>
+		<strong>Error!</strong> No route found matching:
+		<div>
+			<code>{location.pathname}</code>
+		</div>
+	</div>
 );
 
 function App() {
-  return (
-    <div className="App">
-        <HelloMessage name="Everyone" />
-        <HelloMessage name="There" />
-        <br />
-        <Switch>
-            <Route path='/shows' component={ShowList}/>
-            <Route path='/functions' component={FunctionPage}/>
-            <Route path='/classes' component={ClassPage}/>
-            <Route path='/form' component={FormExample}/>
-            <Route exact path='/' render={() => (
-                <Redirect
-                    to='/functions'
-                />
-            )}/>
-            <Route component={NoMatch}/>
-        </Switch>
-    </div>
-  );
+	// this is a test comment from sjchae branch
+
+	return (
+		<div className="App">
+			<HelloMessage name="Everyone" />
+			<HelloMessage name="There" />
+			<br />
+			<Switch>
+				<Route path="/shows" component={ShowList} />
+				<Route path="/functions" component={FunctionPage} />
+				<Route path="/classes" component={ClassPage} />
+				<Route path="/form" component={FormExample} />
+				<Route exact path="/" render={() => <Redirect to="/functions" />} />
+				<Route component={NoMatch} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
